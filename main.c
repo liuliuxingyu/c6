96072f0121275be67552336bd8d6d971f99b674c
邵楠 <1985890648@qq.com>
2019-01-17 16:09:53 +0800

    添加车辆

--- main.c
+++ main.c
@@ -1,5 +1,54 @@
 #include <stdio.h>
+
+struct Point
+{
+    int x;
+    int y;
+};
+
 int main(){
-printf("hello");
+    int index=0;
+    struct Point arr[100];
+printf("huanyingjinrucheliangguanlixitong!\n");
+while(1){
+    printf("1---tianjiacheliang\n");
+    printf("2---shanchucheliang\n");
+    printf("3---xianshisuoyoucheliang\n");
+    printf("4---fanhuiliwozuijindecheliang\n");
+    printf("5---tuichu\n");
+    printf("qingshuruxiangyingdegongnengbianhao:\n");
+    int code;
+    scanf("%d",&code);
+    if(code==1){
+        printf("qingshuruyaotianjiacheliangdexzuobiaoheyzuobiao:\n");
+        int x;
+        int y;
+        scanf("%d",&x);
+        scanf("%d",&y);
+
+        arr[index].x=x;
+        arr[index].y=y;
+
+        index++;
+
+        printf("tianjiachenggong,dianjihuichejixu\n");
+        char m;
+        scanf("%c",&m);
+        scanf("%c",&m);
+
+    }
+    if(code==2){
+        
+    }
+    if(code==3){
+        
+    }
+    if(code==4){
+        
+    }
+    if(code==5){
+        
+    }
+}
     return 0;
 }
